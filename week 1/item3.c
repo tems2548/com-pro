a576acc357646c855a9b5c4c80245cf9c569e8b7
TemZ <110783208+tems2548@users.noreply.github.com>
2024-07-03 22:38:50 -0700

    first commit

---
+++ week 1/item3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main ()
+{
+  printf ("Hello, world!\n");
+  printf ("Hello, Ladkrabang\n");
+  printf ("Hello, Computer Programming\n");
+  printf ("one\ttwo\tthree\tfour\tfive\tHallelujah\n");
+  printf ("12345678\t12345678\t12345678\t12345678\t12345678\t");
+  printf ("\t12345678\t12345678\t12345678\t12345678\t12345678");
+  return 0;
+}
---
+++ week 1/item4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main ()
+{
+  printf ("12345678901234567890123456789012345678901234567890123456789012345678901234567890");
+  printf ("*\t*\t*\t*\t*\t*\t*\t*\t*\t*\t");
+  printf ("Hello, KMITL!\n");
+  printf ("Hello, Ladkrabang.\n");
+  printf ("Hello, Computer Programming.\n");
+  printf ("one\ttwo\tthree\tfour\tfive\tHallelujah\n");
+  return 0;
+}
